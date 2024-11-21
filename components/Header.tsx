884d62641425ed8308880e5a014dd5f92d0662d3
0xcodercrane <codercrane@gmail.com>
2024-11-22 03:25:03 +0800

    update header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { Container } from './Container'
 import Button from './UI/Button'
 
@@ -7,9 +8,15 @@ export default function Header() {
       <Container padding>
         <div className='flex justify-between items-center h-[--header-height] p-5 '>
           <div className='font-bold text-[42px]'>
-            <h1>Doginal Bot</h1>
+            <Link href='/'><h1>Doginal Bot</h1></Link>
           </div>
+          <div className='flex items-center gap-6'>
+            <div className='flex gap-5 text-2xl'>
+              <Link className='hover:text-white duration-200' href="/">Home</Link>
+              <Link className='hover:text-white duration-200' href='/creators'>Creator</Link>
+            </div>
           <Button content='Connect Wallet' />
+          </div>
         </div>
       </Container>
     </header>
